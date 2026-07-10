5cc6c415ad363eab0a7c4b72893c7071f9735086
ikjotkaur-26 <ikjotk485@gmail.com>
2026-07-09 23:20:31 -0700

    Update bankpractices.sql

--- bankpractices.sql
+++ bankpractices.sql
@@ -44,7 +44,29 @@ from european_bank
 order by Balance desc
 limit 5;
 
+
 SELECT Geography,
        ROUND(AVG(Balance), 2) AS Avg_Balance
 FROM european_bank
 GROUP BY Geography;
+
+SELECT CustomerId,
+Balance,
+RANK() OVER(ORDER BY Balance DESC) AS Balance_Rank
+FROM european_bank;
+
+SELECT Geography,
+SUM(Exited) AS Churned
+FROM european_bank
+GROUP BY Geography
+ORDER BY Churned DESC;
+
+SELECT
+CASE
+WHEN Age<30 THEN 'Young'
+WHEN Age BETWEEN 30 AND 50 THEN 'Adult'
+ELSE 'Senior'
+END AS Age_Group,
+COUNT(*) AS Customers
+FROM european_bank
+GROUP BY Age_Group;
